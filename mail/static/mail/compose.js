8a5d851e589b2c6f3889550bc6d0627b5ade6fbc
thenatural86 <philip.smalls86@gmail.com>
2020-11-16 22:46:47 -0500

    post request

--- mail/static/mail/compose.js
+++ mail/static/mail/compose.js
@@ -1,3 +1,25 @@
 document.addEventListener('DOMContentLoaded', () => {
     console.log('hello')
+    const form = document.querySelector('#compose-form');
+    
+    form.addEventListener('submit', e => {
+        e.preventDefault();
+        const recipients = document.querySelector('#compose-recipients').value;
+        const subject = document.querySelector('#compose-subject').value;
+        const body = document.querySelector('#compose-body').value;
+  
+        fetch("/emails", {
+            method: 'POST',
+            body: JSON.stringify({
+                recipients: recipients,
+                subject: subject,
+                body: body
+            }),
+        })
+        .then(response => response.json())
+        .then(result => {
+            // Print result
+            console.log(result);
+        });
+    })
 })
